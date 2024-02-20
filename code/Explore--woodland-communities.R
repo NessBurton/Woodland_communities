6ef4532ccf08ebac299c4db3abd96bc029ffda2c
Vanessa Burton <vanessa.burton26@gmail.com>
2024-02-20 15:52:36 +0000

    Test out comm2sci. List AWIs.

--- code/Explore--woodland-communities.R
+++ code/Explore--woodland-communities.R
@@ -18,6 +18,63 @@ library(taxize)
 
 ### read in data ---------------------------------------------------------------
 
-df_Species <- read.csv(paste0(dirData,"Tree_Species_Handbook_database_Feb24.csv"))
-head(df_Species)
-summary(df_Species)
+# taken from the WT tree species handbook https://www.woodlandtrust.org.uk/publications/2022/03/tree-species-handbook/
+dfSpecies <- read.csv(paste0(dirData,"Tree_Species_Handbook_database_Feb24.csv"))
+head(dfSpecies)
+summary(dfSpecies)
+
+### get scientific names -------------------------------------------------------
+
+# TAKES AGES - BEWARE RUNNING!
+# dfSpecies <- dfSpecies %>% 
+#   mutate(Scientific.name = comm2sci(Species))
+dfSpecies <- dfSpecies %>% 
+  mutate(Scientific.name = ifelse(Scientific.name == "character(0)", NA, Scientific.name))
+
+lstCommon <- unique(dfSpecies$Species)
+lstUID <- get_uid(lstCommon, modifier = "Common Name")
+lstSci <- comm2sci(lstUID)
+
+
+### logical var for ancient woodland indicators --------------------------------
+
+AWIindicators <- c("Woodruff",
+                   "Wood melick",
+                   "Yellow archangel",
+                   "Wych elm",
+                   "Wood spurge",
+                   "Dog's mercury",
+                   "Small-leaved lime",
+                   "Sanicle",
+                   "Herb paris",
+                   "Hairy brome",
+                   "Wood millet",
+                   "Wood sedge",
+                   "Spindle",
+                   "Wood speedwell",
+                   "Lily-of-the-valley",
+                   "Toothwort",
+                   "Goldilocks buttercup",
+                   "Wild service",
+                   "Early dog violet",
+                   "Giant fescue",
+                   "Wood sorrell",
+                   "Slender St. John's-wort",
+                   "Common cow wheat",
+                   "Hairy wood-rush",
+                   "Bluebell",
+                   "Wood anenome",
+                   "Bitter vetch",
+                   "Bilberry",
+                   "Scaly male-fern",
+                   "Hard fern",
+                   "Yellow pimpernel",
+                   "Remote sedge",
+                   "Great wood-rush",
+                   "Opposite-leaved golden-saxifrage",
+                   "Water avens",
+                   "Pendulous sedge",
+                   "Wood horsetail",
+                   "Ramsons",
+                   "Moschatel",
+                   "Red currant")
